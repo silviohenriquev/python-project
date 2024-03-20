673196b311fcf41b5b75307df5fa4bda8ae2a447
Silvio Henrique <shvlpinto@gmail.com>
2024-03-20 19:33:03 -0300

    Broker Publisher

--- lib/communication_embedded/commConfig.py
+++ lib/communication_embedded/commConfig.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as mqtt
 
 version = mqtt.MQTTv5
-MQTT_HOST = "localhost"
+MQTT_HOST = "192.168.3.105"
 MQTT_PORT = 1883
 keepalive = 60
 
--- publisher.py
+++ publisher.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 from lib.sensors import *
 from lib.communication_embedded.MQTTEMQXCommunication import MQTTEMQXCommunication
+from lib.communication_embedded.MQTTEMQXBroker import MQTTEMQXBroker
 from lib.communication_embedded.commTypes import *
 from lib.communication_embedded.commConfig import *
 from time import sleep, time
@@ -20,7 +21,9 @@ packetSensorData = PacketSensorData()
 packetCommandData = PacketCommandData() 
 
 comm = MQTTEMQXCommunication(deviceFunction, deviceId)
+broker = MQTTEMQXBroker()
 
+broker.initializeBroker()
 comm.begin()
 initialTime = time()
 
